04bb14abd1e4e6434d42037874bf7b6fed16ab15
Pallabi <pallabidey768@gmail.com>
2026-01-20 19:40:23 +0530

    session library

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -27,7 +27,7 @@ const CompanionCard = ({id,name,topic,subject,duration,color}:CompanionCardProps
             <Image src="/icons/clock.svg" alt="duration" width={13.5} height={13.5}/>
             <p className="texsm">{duration}minutes</p>
         </div>
-        <Link href={'/companions/${id}'} className="w-full">
+        <Link href={`/companions/${id}`} className="w-full">
             <button className="btn-primary w-full justify-center">
                 Launch Lesson
             </button>
--- lib/actions/companion.action.ts
+++ lib/actions/companion.action.ts
@@ -3,6 +3,8 @@
 // import { auth } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { createSupabaseClient } from "../supabase";
+import error from "next/error";
+// import { error } from "console";
 
 
 export const createCompanion = async(formData:CreateCompanion)=>{
@@ -17,5 +19,39 @@ export const createCompanion = async(formData:CreateCompanion)=>{
     if(error || !data){
         throw new Error(error?.message || 'Failed to create companion');
     }
+    return data;
+}
+
+export const getAllCompanions=async({limit=10,page=1,subject,topic}:GetAllCompanions)=>{
+    const supabase = createSupabaseClient();
+    let query = supabase.from('companions').select('*');
+    if(subject && topic){
+        query=query.ilike('subject', `%${subject}%`)
+        .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+    }else if(subject){
+        query=query.ilike('subject', `%${subject}%`);
+    }else if(topic){
+        query=query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+    }
+
+    query=query.range((page-1)*limit,page*limit-1);
+
+    const {data:companions, error: queryError} = await query;
+    if(queryError) throw new Error(queryError.message);
+
+    return companions;
+}
+
+export const getCompanion = async (id: string) => {
+    const supabase = createSupabaseClient();
+
+    const { data, error } = await supabase
+        .from('companions')
+        .select()
+        .eq('id', id)
+        .single();
+
+    if(error) return console.log(error);
+
     return data;
 }
